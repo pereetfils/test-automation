2385517ebec1fafeb88690b24c574862bbf4f672
MCH <mohammed.chaout@ecoles-epsi.net>
2023-12-01 09:25:51 +0100

    5

--- tests/test_string_service.py
+++ tests/test_string_service.py
@@ -2,17 +2,17 @@ import pytest
 
 from src.services.string_service import to_uppercase
 
-
+@pytest.mark.skip()
 def test_uppercase_with_lowercased_str():
     assert to_uppercase('hello r2-d2') == 'HELLO R2-D2'
 
 def test_uppercase_with_uppercased_str():
     assert to_uppercase('HELLO YOU R2D2') == 'HELLO YOU R2D2'
 
-
+@pytest.mark.skip
 def test_uppercase_with_no_str():
     assert to_uppercase('') == ''
 
-
+@pytest.mark.skip
 def test_uppercase_with_not_str():
     assert to_uppercase(2) == 2
