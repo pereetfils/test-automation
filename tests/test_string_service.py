674d0685aa8990aa29626d2ebde52fe3c24508dc
MCH <mohammed.chaout@ecoles-epsi.net>
2023-12-01 13:33:47 +0100

    lili

--- tests/test_string_service.py
+++ tests/test_string_service.py
@@ -7,7 +7,7 @@ def test_uppercase_with_lowercased_str():
     assert to_uppercase('hello r2-d2') == 'HELLO R2-D2'
 
 def test_uppercase_with_uppercased_str():
-    assert to_uppercase('HI YOU 2D2') == 'HI YOU R2D2'
+    assert to_uppercase('HI YOU R2D2') == 'HI YOU R2D2'
 
 @pytest.mark.skip
 def test_uppercase_with_no_str():
